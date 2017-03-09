e01d545bbc95db4194fc0e894eab568a61170803
tfennelly <tom.fennelly@gmail.com>
2017-03-09 15:56:45 +0000

    panel vue-play story

--- play/Panel.vue
+++ play/Panel.vue
@@ -1,5 +1,104 @@
 <template>
     <Page>
-        <button type="button" class="btn btn-primary" @click="handleClick">Hello</button>
+        <div id="Panel">
+            <LogCategories :loadCategoriesFunc="loadCategoriesFunc"></LogCategories>
+        </div>
     </Page>
 </template>
+
+<script>
+    import LogCategories from '../src/components/LogCategories.vue'
+
+    export default {
+        name: 'Panel',
+        components: {
+            LogCategories
+        },
+        methods: {
+            loadCategoriesFunc: function(callback) {
+                callback({
+                    "count": 19,
+                    "categories": [
+                        {
+                            "category": "io.jenkins.blueocean.core.LiveStatusIndicator",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.core.ParametersRunButton",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.core.TimeHarmonizer",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.InputStep",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.LogConsole",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.PipelinePage",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.PipelineRunGraph",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.RunDetails",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.RunDetailsPipeline",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.Runs",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.Step",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.artifacts",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.dashboard.harmonizeTimes",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.github-pipeline",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.i18n",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.i18n.startup",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.jsextensions.ExtensionStore",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "io.jenkins.blueocean.web.routing",
+                            "logLevel": "_"
+                        },
+                        {
+                            "category": "org.jenkinsci.sse",
+                            "logLevel": "_"
+                        }
+                    ],
+                    "_$eventId": "log.categories"
+                });
+            }
+        }
+    }
+</script>
--- play/index.js
+++ play/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue';
 import Page from './infra/Page.vue'
 
+Vue.use(BootstrapVue);
 Vue.component('Page', Page);
 
 require('./panel.play');
